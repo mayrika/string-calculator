7645077e93a3a7878ff9346626fb81cbebbc5301
mayrika <mayrika@mayrikas-MacBook-Air.local>
2018-06-04 16:20:22 +0700

    Add case test

--- Calculator.js
+++ Calculator.js
@@ -1,7 +1,17 @@
 class Calculator {
     calculate(str) {
-        if(str==='')
-        return 0;
+        if(str.indexOf(",")!==-1) {
+            var ans = str.split(",")
+            var sum = parseInt(ans[0])+parseInt(ans[1])
+            return sum }
+        if(str.indexOf("\n")!==-1) {
+            var ans = str.split("\n")
+            var sum1 = parseInt(ans[0])+parseInt(ans[1])
+            return sum1 }
+        else if (str) {
+            return parseInt(str) }
+        else if(str===''){
+            return 0 }
     }
 }
 
