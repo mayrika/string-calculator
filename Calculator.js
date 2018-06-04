ec8c20371b479abca1bd4ea7a5b35adfa2abedf0
Tharinee Munkwamdee <tharineemunk@Tharinees-MacBook-Air.local>
2018-06-04 17:28:54 +0700

    delim comma newline more than 2 arguments

--- Calculator.js
+++ Calculator.js
@@ -1,18 +1,23 @@
 class Calculator {
-    calculate(str) {
-        if(str.indexOf(",")!==-1) {
-            var ans = str.split(",")
-            var sum = parseInt(ans[0])+parseInt(ans[1])
-            return sum }
-        if(str.indexOf("\n")!==-1) {
-            var ans = str.split("\n")
-            var sum1 = parseInt(ans[0])+parseInt(ans[1])
-            return sum1 }
-        else if (str) {
-            return parseInt(str) }
-        else if(str===''){
-            return 0 }
-    }
+  constructor() {
+    this.str = '';
+  }
+  calculate(str) {
+    this.str = str;
+    if (str.indexOf(',') !== -1) {
+      const ans = str.split(',');
+      let sum1 = 0;
+      for (let i = 0; i < ans.length; i += 1) sum1 += parseInt(ans[i], 10);
+      return sum1;
+    } else if (str.indexOf('\n') !== -1) {
+      const ans = str.split('\n');
+      let sum2 = 0;
+      for (let j = 0; j < ans.length; j += 1) sum2 += parseInt(ans[j], 10);
+      return sum2;
+    } else if (str) return parseInt(str, 10);
+    else if (str === '') return 0;
+    return -1;
+  }
 }
 
-module.exports = Calculator
+module.exports = Calculator;
--- test/test-string-calculator.js
+++ test/test-string-calculator.js
@@ -1,63 +1,65 @@
-const Calculator = require('../Calculator')
-const chai = require('chai')
+const Calculator = require('../Calculator');
+const chai = require('chai');
 
-const { expect } = chai
+const { expect } = chai;
 
 // Start test cases for Example Behaviors
 function itAlwaysTrue() {
 // Chai API Reference can be found at http://www.chaijs.com/api/bdd/
-  expect(true).to.be.true
+  expect(true).to.be.true;
 }
 
 function itAlwaysBe2() {
-  expect(2).to.be.equal(2)
+  expect(2).to.be.equal(2);
 }
 
 function itShouldBeNewline() {
-  const result1 = new Calculator()
-  expect(result1.calculate('1\n2')).to.be.equal(3)
+  const result = new Calculator();
+  expect(result.calculate('1\n2')).to.be.equal(3);
+  expect(result.calculate('1\n2\n3')).to.be.equal(6);
 }
 
 function itShouldBeComma() {
-  const result = new Calculator()
-  expect(result.calculate('1,2')).to.be.equal(3)
+  const result = new Calculator();
+  expect(result.calculate('1,2')).to.be.equal(3);
+  expect(result.calculate('1,2,3')).to.be.equal(6);
 }
 
 function itShouldBeNumber() {
-  const num = new Calculator()
-  expect(num.calculate('89')).to.be.equal(89)
+  const num = new Calculator();
+  expect(num.calculate('89')).to.be.equal(89);
 }
 
 function itShouldBeZero() {
-  const cal1 = new Calculator()
-  expect(cal1.calculate('')).to.be.equal(0)
+  const cal1 = new Calculator();
+  expect(cal1.calculate('')).to.be.equal(0);
 }
 
 // Start Example Behaviors
 function exampleBehaviors() {
-  it('should be true', itAlwaysTrue)
-  it('should be equal 2', itAlwaysBe2)
+  it('should be true', itAlwaysTrue);
+  it('should be equal 2', itAlwaysBe2);
 }
 
-function testNewlinedelimited() {  
-  it('should be newline', itShouldBeNewline)
+function testNewlinedelimited() {
+  it('should be newline', itShouldBeNewline);
 }
 
-function testCommadelimited() {  
-  it('should be comma', itShouldBeComma)
+function testCommadelimited() {
+  it('should be comma', itShouldBeComma);
 }
 
-function testSinglenumber() {  
-  it('should be number', itShouldBeNumber)
+function testSinglenumber() {
+  it('should be number', itShouldBeNumber);
 }
 
-function testEmptyString() {  
-  it('should be zero', itShouldBeZero)
+function testEmptyString() {
+  it('should be zero', itShouldBeZero);
 }
 
 // Start Describe
-describe('Example', exampleBehaviors)
-describe('Empty string', testEmptyString)
-describe('single number', testSinglenumber)
-describe('comma delimited', testCommadelimited)
-describe('newline delimited', testNewlinedelimited)
+describe('Example', exampleBehaviors);
+describe('Empty string', testEmptyString);
+describe('single number', testSinglenumber);
+describe('comma delimited', testCommadelimited);
+describe('newline delimited', testNewlinedelimited);
